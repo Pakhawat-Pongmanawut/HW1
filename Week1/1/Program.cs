79ad43eedc282cc8dbd72cc6f770b5d9ef11090f
Pakhawat-Pongmanawut <110761105+Pakhawat-Pongmanawut@users.noreply.github.com>
2023-02-06 20:39:14 +0700

    Add files via upload

---
+++ Week1/1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+    {
+     public static void Main()
+     {
+        double n;
+        n = double.Parse(Console.ReadLine());
+        Console.WriteLine(PFunc(n));
+     }
+    public static double PFunc(double n)
+    {
+        if ((Math.Pow(n,2)) <= 4)
+        {
+            return 2;
+        }
+        else
+        {
+            return (1 + PFunc(n - (4 * (Math.Abs(n)) / n)));
+        }
+    }
+    }
---
+++ Week1/3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+    {
+     public static void Main()
+     {
+        double n;
+        n = double.Parse(Console.ReadLine());
+        Console.WriteLine(PFunc(n));
+     }
+    public static double PFunc(double n)
+    {
+        if ((n > -1) && (n < 0))
+        {
+            return (Math.Sqrt(1 - (Math.Pow(n,2))));;
+        }
+        else if ((n > 0) && (n < 1))
+        {
+            return -(Math.Sqrt(1 - (Math.Pow(n,2))));
+        }
+        else if (n <= -1)
+        {
+            return PFunc(n + 2);
+        }
+        else
+        {
+            return PFunc(n - 2);
+        }
+    }
+    }
